7bc9be0dbb61177b917ab74aeff95600717581e4
Sindhu arju <sindhuarju19@gmail.com>
2018-07-31 12:48:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/gmamdin/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gmamdin.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Name=(EditText)findViewById(R.id.etName);
+        Password=(EditText)findViewById(R.id.etPassword);
+        Login=(Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+    private void validate(String username,String password){
+        if( (username.equals("admin")) && (password.equals("admin")) ){
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
